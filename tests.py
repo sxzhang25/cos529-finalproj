85742c7ab7181b59d8384c2061e479018c0ff3fc
Sharon Zhang <zsharonx@gmail.com>
2020-05-03 00:47:12 -0400

    rewrite tests

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ from mnist import MNIST
 import naive_nn as nnn
 import twin_nn as tnn
 from preprocessing import *
-from oneshot import *
+# from oneshot import *
 
 np.random.seed(0) # set seed
 
@@ -21,7 +21,7 @@ n_classes, n_examples, w, h = X.shape
 X_test, y_test, alphabet_dict_test, _ = load_imgs('./data/omniglot/images_evaluation')
 
 # tests
-tests = ['twin_nn']
+tests = ['naive_nn']
 pretrained = True
 
 
@@ -37,40 +37,8 @@ for test in tests:
     naive_nn.plot()
 
   elif test == 'naive_nn':
-    mndata = MNIST('data/mnist')
-    scaler = StandardScaler()
-    cpc = 2  # centers per class
-
-    # prepare anchors
-    anchors, anchor_labels = mndata.load_training()
-    anchor_labels = np.array(anchor_labels)
-
-    anchor_idxs = np.zeros((0,), dtype='int')
-    for i in range(10):
-      idxs = np.where(anchor_labels==i)[0]
-      new_idxs = np.array([idxs[i] for i in np.random.randint(0, len(idxs), cpc)])
-      anchor_idxs = np.concatenate((anchor_idxs, new_idxs), axis=0)
-
-    anchors = np.array([anchors[idx] for idx in anchor_idxs]).T
-    anchor_labels = np.array([anchor_labels[idx] for idx in anchor_idxs])
-
-    test_data, test_labels = mndata.load_testing()  # load test data
-    test_data = np.array(test_data).T
-    test_labels = np.array(test_labels)
-
-    index = np.random.randint(0, len(anchor_labels))  # choose an index
-    print(mndata.display(anchors.T[index]))
-
-    pca = PCA(n_components=2, svd_solver='arpack')
-    pca.fit(np.hstack((anchors, test_data)))
-    anchors_, test_data_ = pca.components_.T[:10 * cpc], pca.components_.T[10 * cpc:]
-
-    naive_nn = nnn.Naive_NN(anchors_, anchor_labels)
-    y_ = naive_nn.classify(test_data_)
-
-    naive_nn.plot()
-
-    print('\nAccuracy: %2.2f %%' % (100 * len(np.where(y_==test_labels)[0]) / len(y_)))
+    for i in range(2, 11):
+      nnn.test_oneshot(i, 10, X_test, y_test, alphabet_dict_test, language=None, verbose=1)
 
   elif test == 'twin_nn':
     if not pretrained:
@@ -89,5 +57,5 @@ for test in tests:
     else:
       twin_nn = keras.models.load_model('models/twin_nn')
 
-      for i in range(1, 11):
+      for i in range(2, 11):
         tnn.test_oneshot(twin_nn, i, 500, X_test, y_test, alphabet_dict_test, language=None, verbose=1)
