ae5eb31fb4fda4c6ca60b3bfd2ba90004e584e83
Sharon Zhang <zsharonx@gmail.com>
2020-05-03 00:46:48 -0400

    remove test function

--- oneshot.py
+++ oneshot.py
@@ -29,23 +29,3 @@ def create_oneshot_task(X, labels, alphabet_dict, N=1, language=None):
   pairs = [test_img, support_imgs]
 
   return pairs, targets
-
-def test_oneshot(model, N, k, data, labels, alphabet_dict, language=None, verbose=0):
-  '''
-  Test average N-way oneshot learning accuracy of model over k one-shot tasks
-  '''
-  correct = 0
-  if verbose:
-    print("Evaluating model on {} random {}-way one-shot learning tasks...".format(k,N))
-
-  for i in range(k):
-    inputs, targets = create_oneshot_task(data, labels, alphabet_dict, N=N, language=language)
-    probs = model.predict(inputs)
-    if np.argmax(probs) == np.argmax(targets):
-      correct += 1
-
-  accuracy = (100 * correct / k)
-  if verbose:
-    print("Average %d-way one-shot accuracy: %4.2f%%" % (N, accuracy))
-
-  return accuracy
